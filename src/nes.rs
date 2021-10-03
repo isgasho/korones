7d819d1490aa56b2b37390e42b8b63109031cccc
thara <git@thara.dev>
2021-10-03 21:57:52 +0900

    :muscle: Implement CpuBus for Bus

--- src/nes.rs
+++ src/nes.rs
@@ -1,4 +1,4 @@
-use crate::cpu::Cpu;
+use crate::cpu::{Cpu, CpuBus};
 use crate::mapper::{Empty, Mapper};
 
 #[derive(Debug)]
@@ -26,4 +26,26 @@ impl Nes {
 pub(crate) enum Mirroring {
     Horizontal,
     Vertical,
+}
+
+pub(crate) struct Bus {}
+
+impl CpuBus for Bus {
+    fn read(nes: &mut Nes, addr: u16) -> u8 {
+        match addr {
+            0x0000..=0x07FF => nes.wram[addr as usize],
+            0x0800..=0x1FFF => nes.mapper.read(addr - 0x0800),
+            //TODO ppu, apu, controllers
+            _ => 0,
+        }
+    }
+
+    fn write(nes: &mut Nes, addr: u16, value: u8) {
+        match addr {
+            0x0000..=0x07FF => nes.wram[addr as usize] = value,
+            0x0800..=0x1FFF => nes.mapper.write(addr - 0x0800, value),
+            //TODO ppu, apu, controllers
+            _ => {}
+        }
+    }
 }
